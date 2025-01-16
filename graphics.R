a025baa4cfff3c2be44a14c362088fbd16463395
“Crimaco197” <crmartinezco@unal.edu.co>
2025-01-16 09:53:00 +0100

    segunda parte

--- graphics.R
+++ graphics.R
@@ -1,6 +1,14 @@
 #Data exploration
 data_address <- "C:/Users/luzve/Documents/INSA 5eme annee/Visualisation des donnees/R-report/IEFIC_2018.csv"
+data_address <- "/Users/cristianmartinez/Library/Mobile Documents/com~apple~CloudDocs/UNIVERSIDADES/INSA TOULOUSE/Analyse Exploratoire et Visualisation de donnees/R-report/IEFIC_2018.csv"
 data <- read.csv(data_address, header = TRUE, sep = ";", stringsAsFactors = FALSE)
+
+# Libraries
+library(dplyr)
+library(ggplot2)
+library(stringr)
+library(treemapify)
+library(tidyr)
 
 # Clean data
 # We'll use just the two first parts of the survey
@@ -32,13 +40,38 @@ colnames(data)[colnames(data) == "P2478_11"] <- "school_transport_expense"
 colnames(data)[colnames(data) == "P2478_12"] <- "pension_expense"
 colnames(data)[colnames(data) == "P2479"] <- "income_vs_expenses"
 
+colnames(data)[colnames(data) == "P2481_1"] <- "housing_saving"
+colnames(data)[colnames(data) == "P2481_2"] <- "home_equipment"
+colnames(data)[colnames(data) == "P2481_3"] <- "jewelry"
+colnames(data)[colnames(data) == "P2481_4"] <- "art"
+colnames(data)[colnames(data) == "P2481_5"] <- "future_rent"
+colnames(data)[colnames(data) == "P2481_6"] <- "vacations"
+colnames(data)[colnames(data) == "P2481_7"] <- "retirement"
+colnames(data)[colnames(data) == "P2481_8"] <- "emergency"
+colnames(data)[colnames(data) == "P2481_9"] <- "future_expenses"
+colnames(data)[colnames(data) == "P2481_10"] <- "education"
+colnames(data)[colnames(data) == "P2481_11"] <- "debt_reduction"
+colnames(data)[colnames(data) == "P2481_12"] <- "medical_treatment"
+colnames(data)[colnames(data) == "P2481_13"] <- "children"
+colnames(data)[colnames(data) == "P2481_14"] <- "weddings"
+colnames(data)[colnames(data) == "P2481_15"] <- "investment"
+colnames(data)[colnames(data) == "P2481_16"] <- "inheritance"
+colnames(data)[colnames(data) == "P2481_17"] <- "home_renovation"
+colnames(data)[colnames(data) == "P2481_18"] <- "savings"
+
+colnames(data)[colnames(data) == "P2482_1"] <- "strategy_debt"
+colnames(data)[colnames(data) == "P2482_2"] <- "strategy_asset_sale"
+colnames(data)[colnames(data) == "P2482_3"] <- "strategy_savings"
+colnames(data)[colnames(data) == "P2482_4"] <- "strategy_family_help"
+colnames(data)[colnames(data) == "P2482_5"] <- "strategy_other"
+colnames(data)[colnames(data) == "P2471_4"] <- "mortgage_balance"
+
+
 head(data)
 summary(data)
 dim(data)
 
 # Graphic 1: Is income enough related to be a homeowner or not (paying or not rent) ?
-library(dplyr)
-library(ggplot2)
 
 # Filter and group data to count
 data_summary <- data %>%
@@ -70,8 +103,6 @@ ggplot(data_summary, aes(x = income_vs_expenses_label, y = count, fill = home_ow
 
 
 # Graphic 2 : Treemap of expenses by category
-library(treemapify)
-library(tidyr)
 
 # COP to USD in 2018 (1USD=3900COP)
 usd_conversion_rate <- 0.00034  
@@ -165,6 +196,94 @@ ggplot(expenses_long, aes(x = category, y = amount, fill = income_vs_expenses_la
     legend.text = element_text(size = 8)  
   )
 
+" Saving and Consumption Pattern by Homeownership "
+
+
+
+
+# Generate the savings dataset with homeownership
+savings_data <- data %>%
+  select(
+    home_owner,
+    housing_saving, home_equipment, jewelry, art, future_rent, vacations, 
+    retirement, emergency, future_expenses, education, debt_reduction, 
+    medical_treatment, children, weddings, investment, inheritance, 
+    home_renovation, savings
+  ) %>%
+  mutate(
+    home_owner_label = ifelse(home_owner == 1, "Homeowner", "Non-homeowner")
+  ) %>%
+  group_by(home_owner_label) %>%
+  summarise(across(
+    housing_saving:savings,
+    ~ sum(. == 1, na.rm = TRUE)
+  )) %>%
+  pivot_longer(cols = -home_owner_label, names_to = "category", values_to = "count") %>%
+  group_by(home_owner_label) %>%
+  mutate(percentage = count / sum(count) * 100)
+
+
+# Prepare data for graphic
+savings_curve_data <- savings_data %>%
+  group_by(home_owner_label) %>%
+  mutate(category = factor(category, levels = unique(category))) %>%
+  ungroup()
+
+
+
+ggplot(savings_curve_data, aes(x = category, y = percentage, fill = home_owner_label)) +
+  geom_bar(stat = "identity", position = "dodge", color = "black") +  
+  labs(
+    x = "Expense Savings Category",
+    y = "Percentage of Households",
+    fill = "Homeownership"
+  ) +
+  theme_minimal() +
+  theme(
+    axis.text.x = element_text(angle = 45, hjust = 1),  
+    legend.position = "top"
+  )
+
+" Strategies for Covering Deficits by Mortgage Level "
+
+# Clasify the total mortgage level in USD
+data <- data %>%
+  mutate(
+    mortgage_level = case_when(
+      mortgage_balance <= 50000000 ~ "Low Debt < $17k",
+      mortgage_balance > 50000000 & mortgage_balance <= 150000000 ~ "$17k < Medium Debt < $51k",
+      mortgage_balance > 150000000 ~ "High Debt > $51k",
+      TRUE ~ NA_character_  # Ignore empty fields
+    )
+  ) %>%
+  filter(!is.na(mortgage_level))
+
+# Filter only the needed data to make the graphic 
+strategies_data <- data %>%
+  filter(mortgage_level %in% c("Low Debt < $17k", "$17k < Medium Debt < $51k", "High Debt > $51k")) %>%  # Incluir solo los niveles válidos
+  select(mortgage_level, strategy_debt, strategy_asset_sale, strategy_savings, strategy_family_help, strategy_other) %>%
+  pivot_longer(cols = starts_with("strategy_"), names_to = "strategy", values_to = "used") %>%
+  filter(used == 1) 
+
+# clean parameter name
+strategies_data <- strategies_data %>%
+  mutate(strategy = str_replace(strategy, "strategy_", "")) 
+
+
+ggplot(strategies_data, aes(x = mortgage_level, fill = strategy)) +
+  geom_bar(position = "fill", color = "black") +
+  scale_y_continuous(labels = scales::percent_format()) +
+  labs(
+   # title = "Strategies for Covering Deficits by Mortgage Level",
+    x = "Mortgage Level",
+    y = "Proportion of Strategies Used",
+    fill = "Strategy"
+  ) +
+  theme_minimal() +
+  theme(
+    axis.text.x = element_text(angle = 45, hjust = 1),
+    legend.position = "top"
+  )
 
 
 
